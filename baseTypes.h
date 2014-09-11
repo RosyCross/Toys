847693e56e6059c87ae620ad895a3dd9731102f7
Charlie Root <root@ReCon.localdomain>
2014-09-11 15:55:30 +0800

    Add the necessary include files for type limits.

--- baseTypes.h
+++ baseTypes.h
@@ -1,6 +1,10 @@
 #ifndef _BASETYPES_H_
 #define _BASETYPES_H_
 
+#ifndef _LIMITS_H___
+    #include <limits.h>
+#endif
+
 typedef int IntType;
 const IntType IntTypeMax = INT_MAX;
 const IntType IntTypeMin = INT_MIN;
